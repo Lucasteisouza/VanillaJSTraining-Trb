7c4fa6a7f96df4bcfd7ca923caaa13edf2c0349f
Lucasteisouza <lucasteisouza@gmail.com>
2022-09-29 17:33:55 -0300

    Requisito 4 atendido

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  let isManag = false;
+  const { employees } = data;
+  employees.forEach((element) => {
+    const { managers } = element;
+    if (managers.some((manager) => manager === id)) {
+      isManag = true;
+    }
+  });
+  return isManag;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const { employees } = data;
+  if (isManager(managerId)) {
+    const downManages = [];
+    employees.forEach((employee) => {
+      if (employee.managers.includes(managerId)) {
+        downManages.push(`${employee.firstName} ${employee.lastName}`);
+      }
+    });
+    return downManages;
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
