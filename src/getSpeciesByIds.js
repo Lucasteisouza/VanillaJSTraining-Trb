433b5211a5d7a4ed383573d7c1ae6977df429277
Lucasteisouza <lucasteisouza@gmail.com>
2022-09-29 15:28:51 -0300

    Requisito 1 atendido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const { species } = data;
+  const selectedIds = [];
+  ids.forEach((element) =>{
+    selectedIds.push(species.find((specie) => specie.id === element))
+  });
+  return selectedIds;
 }
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
