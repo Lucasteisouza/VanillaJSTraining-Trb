c05054f3c1a05d9451d61b219f5e19445804d922
Lucasteisouza <lucasteisouza@gmail.com>
2022-09-29 19:22:26 -0300

    Requisito 5 atendido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const selectedAnimal = (anyAnimal) => {
+  const speciesObj = species.find((element) => anyAnimal.specie === element.name);
+  if (anyAnimal.sex === undefined) {
+    return speciesObj.residents.length;
+  } const { residents } = speciesObj;
+  return residents.filter((element) => element.sex === anyAnimal.sex).length;
+};
+
 function countAnimals(animal) {
-  // seu código aqui
+  const allAnimalsCounter = {};
+  if (animal === undefined) {
+    species.forEach((element) => {
+      allAnimalsCounter[element.name] = element.residents.length;
+    });
+    return allAnimalsCounter;
+  } return selectedAnimal(animal);
 }
 
+// console.log(selectedAnimal({ specie: 'lions', sex: 'male' }));
+// console.log(countAnimals({ specie : 'lions'}));
 module.exports = countAnimals;
