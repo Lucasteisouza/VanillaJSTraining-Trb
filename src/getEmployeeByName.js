9b748b6411195cca5de8ba5144f76e517b9c4019
Lucasteisouza <lucasteisouza@gmail.com>
2022-09-29 17:45:41 -0300

    Requisito 3 atendido

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const { employees } = data;
+  const isReal = employees.find((e) => e.firstName === employeeName || e.lastName === employeeName);
+  if (isReal === undefined) {
+    return {};
+  } return isReal;
 }
 
 module.exports = getEmployeeByName;
