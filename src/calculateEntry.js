cfed3f5203e9ee209d4408b84f997c8491fe0082
Lucasteisouza <lucasteisouza@gmail.com>
2022-09-29 21:06:29 -0300

    Requisito 8 atendido

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const childArr = entrants.filter((element) => element.age < 18);
+  const adultArr = entrants.filter((element) => element.age >= 18 && element.age < 50);
+  const seniorArr = entrants.filter((element) => element.age >= 50);
+  return { child: childArr.length, adult: adultArr.length, senior: seniorArr.length };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined) {
+    return 0;
+  } if (entrants.length === 0) {
+    return 0;
+  } if (Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const entNumber = countEntrants(entrants);
+  return (entNumber.child * 20.99) + (entNumber.adult * 49.99) + (entNumber.senior * 24.99);
 }
 
 module.exports = { calculateEntry, countEntrants };
